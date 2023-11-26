949c999dda1ba89e09cd1e1dc548387ba710251f
es-e2b <127708801+es-e2b@users.noreply.github.com>
2023-11-27 01:04:26 +0900

    [Silver III] Title: N과 M (1), Time: 264 ms, Memory: 22756 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -6,40 +6,37 @@ public class Main {
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	static StringBuilder sb=new StringBuilder();
 	static StringTokenizer st;
-	static int[] arr;
 	static int[] ans;
+	static boolean[] visited;
 	static int n;
 	static int m;
 	public static void main(String[] args) throws IOException {
 		st=new StringTokenizer(br.readLine());
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
-		arr=new int[n];
-		ans=new int[m];
-		for(int i=0;i<n;i++)
-			arr[i]=i+1;
-		f(0);
+		ans=new int[m+1];
+		visited=new boolean[n+1];
+		f(1);
 		bw.append(sb);
         bw.flush();
         bw.close();
         br.close();
 	}
 	static void f(int count) {
-		if(count==m) {
-			for(int i : ans)
-				sb.append(i+" ");
-			sb.append('\n');
-			return;
-		}
-		for(int i=0;i<n;i++) {
-			boolean isUsed=false;
-			for(int j=0;j<count;j++)
-				if(arr[i]==ans[j])
-					isUsed=true;
-			if(isUsed)
-				continue;
-			ans[count]=arr[i];
-			f(count+1);
-		}
+        if (count==m+1) {
+            for (int i=1; i<=m; i++) {
+                sb.append(ans[i]).append(' ');
+            }
+            sb.append('\n');
+        } else {
+            for (int i=1; i<=n; i++) {
+                if (!visited[i]) {
+                    ans[count]=i;
+                    visited[i]=true;
+                    f(count+1);
+                    visited[i]=false;
+                }
+            }
+        }
 	}
 }
