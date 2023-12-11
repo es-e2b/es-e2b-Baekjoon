511530b3340947d48aff8f9daddb891c64047057
es-e2b <127708801+es-e2b@users.noreply.github.com>
2023-12-11 21:24:41 +0900

    [Bronze V] Title: 제리와 톰, Time: 152 ms, Memory: 16092 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+import java.util.Map.Entry;
+	
+public class Main {
+	static int[] dx=new int[] {1,-1,0,0};
+	static int[] dy=new int[] {0,0,1,-1};
+	private static int read() throws Exception{
+	    int c, n = System.in.read() & 15;
+	    while ((c = System.in.read()) > 32)
+	    	n = (n << 3) + (n << 1) + (c & 15);
+	    return n;
+	}
+	static StringTokenizer st;
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringBuilder sb=new StringBuilder();
+	public static void main(String[] args) throws Exception {
+		int a=read();
+		int b=read();
+		if(a!=b)
+			sb.append(b-a+" "+b);
+		else
+			sb.append(0);
+		
+		bw.append(sb);
+        bw.flush();
+        bw.close();
+        br.close();
+	}
+}
