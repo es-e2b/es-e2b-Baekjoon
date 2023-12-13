5ba9e5598d238a58d3369d26fcde93336fd9f7a6
es-e2b <127708801+es-e2b@users.noreply.github.com>
2023-12-13 22:34:16 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 124 ms, Memory: 14364 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+import java.util.Map.Entry;
+	
+public class Main {
+	static int[] dx=new int[] {1,-1,0,0};
+	static int[] dy=new int[] {0,0,1,-1};
+	private static int read() throws Exception{
+	    int c, n = System.in.read() & 15;
+	    while ((c = System.in.read()) > 32)
+	    	n = (n << 3) + (n << 1) + (c & 15);
+	    return n;
+	}
+	static StringTokenizer st;
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringBuilder sb=new StringBuilder();
+	public static void main(String[] args) throws Exception {
+		int T=read();
+		HashMap<String, String> map = new HashMap<>();
+		map.put("Algorithm", "204");
+		map.put("DataAnalysis", "207");
+		map.put("ArtificialIntelligence", "302");
+		map.put("CyberSecurity", "B101");
+		map.put("Network", "303");
+		map.put("Startup", "501");
+		map.put("TestStrategy", "105");
+		while(T-->0)
+			sb.append(map.get(br.readLine())+"\n");
+		
+		bw.append(sb);
+        bw.flush();
+        bw.close();
+        br.close();
+	}
+}
