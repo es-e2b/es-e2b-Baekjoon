a11f740468a263d522377196ddbc8f0656df49a0
es-e2b <127708801+es-e2b@users.noreply.github.com>
2023-12-28 13:39:55 +0900

    [Gold V] Title: 평범한 배낭, Time: 168 ms, Memory: 14628 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,9 +1,5 @@
 import java.io.*;
-import java.util.*;
 public class Main {
-	static int N, K;
-	static int[][] W;
-	static int[][] V;
 	private static int read() throws Exception{
 	    int c, n = System.in.read() & 15;
 	    while ((c = System.in.read()) > 32)
@@ -11,30 +7,24 @@ public class Main {
 	    return n;
 	}
 	static StringBuilder sb=new StringBuilder();
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	public static void main(String[] args) throws Exception {
-		N=read();
-		K=read();
-		W=new int[N][2];
-		V=new int[N][K+1];
+		int N=read();
+		int K=read();
+		int[] W=new int[N];
+		int[] V=new int[N];
+		int[] dp=new int[K+1];
 		for(int i=0;i<N;i++) {
-			W[i][0]=read();
-			W[i][1]=read();
-		}
-		for(int i=W[0][0];i<=K;i++) {
-			V[0][i]=W[0][1];
-		}
-		for(int i=1;i<N;i++) {
-			V[i]=V[i-1].clone();
-			for(int j=K;j>=W[i][0];j--) {
-				V[i][j]=Math.max(V[i][j], W[i][1]+V[i][j-W[i][0]]);
+			W[i]=read();
+			V[i]=read();
+			for(int j=K;j>=0;j--) {
+				if(j+W[i]>K)continue;
+				dp[j+W[i]]=Math.max(dp[j+W[i]], dp[j]+V[i]);
 			}
 		}
-		sb.append(V[N-1][K]);
+		sb.append(dp[K]);
 		bw.append(sb);
         bw.flush();
         bw.close();
-        br.close();
 	}
 }
