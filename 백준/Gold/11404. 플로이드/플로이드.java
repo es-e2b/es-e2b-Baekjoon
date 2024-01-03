b891ffd272292824f45efe267c61308b0aaf902d
es-e2b <127708801+es-e2b@users.noreply.github.com>
2024-01-03 20:39:25 +0900

    [Gold IV] Title: 플로이드, Time: 664 ms, Memory: 45748 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -15,10 +15,6 @@ public class Main {
     public static void main(String[] args) throws Exception {
     	N=read(); M=read();
     	cost=new int[N+1][N+1];
-    	for(int i=0;i<=N;i++) {
-    		Arrays.fill(cost[i], -1);
-    		cost[i][i]=0;
-    	}
     	for(int i=0;i<M;i++) {
     		int start=read();
     		int end=read();
@@ -33,23 +29,17 @@ public class Main {
     		q.add(new int[] {i,0});
     		while(!q.isEmpty()) {
     			int[] now=q.poll();
-//    			System.out.println("\n now : "+now[0]+" "+now[1]);
-    			if(cost[i][now[0]]==-1||now[1]<cost[i][now[0]])
+    			if(cost[i][now[0]]<=0||now[1]<cost[i][now[0]])
     				cost[i][now[0]]=now[1];
     			for(int k=1;k<=N;k++) {
     				int newCost=now[1]+cost[now[0]][k];
-//    				if(cost[now[0]][k]>0)
-//    					System.out.println("new : "+k+" "+newCost);
     				if(now[0]==k||k==i||cost[now[0]][k]<=0||cost[i][k]>0&&cost[i][k]<newCost)continue;
-//    				System.out.println("add : "+k+" "+newCost);
     				q.offer(new int[] {k,newCost});
     			}
     		}
     	}
     	for(int i=1;i<=N;i++) {
     		for(int j=1;j<=N;j++) {
-    			if(cost[i][j]==-1)
-    				cost[i][j]=0;
     			sb.append(cost[i][j]+" ");
     		}
     		sb.append("\n");
