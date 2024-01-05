575c7726d6cf0ae27df2a6dd8fbbd8d9cc0f07d2
es-e2b <127708801+es-e2b@users.noreply.github.com>
2024-01-05 16:46:19 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 124 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32)
+            n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
+    public static void main(String[] args) throws Exception {
+    	int n=read();
+    	long[] dp=new long[n+1];
+    	dp[0]=0;
+    	dp[1]=1;
+    	for(int i=2;i<=n;i++) {
+    		dp[i]=dp[i-1]+dp[i-2];
+    	}
+    	sb.append(dp[n]);
+        bw.append(sb);
+        bw.flush();
+        bw.close();
+    }
+}
