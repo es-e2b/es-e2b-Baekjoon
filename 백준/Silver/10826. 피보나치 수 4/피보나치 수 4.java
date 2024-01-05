7b291452629931e429f4f0c72dfbfe4232b8fe39
es-e2b <127708801+es-e2b@users.noreply.github.com>
2024-01-05 17:02:36 +0900

    [Silver V] Title: 피보나치 수 4, Time: 180 ms, Memory: 21536 KB -BaekjoonHub

---
+++ 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32)
+            n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
+    public static void main(String[] args) throws Exception {
+    	int n=read();
+    	BigInteger[] dp=new BigInteger[n+2];
+    	dp[0]=BigInteger.ZERO;
+    	dp[1]=BigInteger.ONE;
+    	for(int i=2;i<=n;i++) {
+    		dp[i]=dp[i-1].add(dp[i-2]);
+    	}
+    	sb.append(dp[n]);
+        bw.append(sb);
+        bw.flush();
+        bw.close();
+    }
+}
