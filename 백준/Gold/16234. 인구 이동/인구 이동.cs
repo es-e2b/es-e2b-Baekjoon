7b3a36681f5a3ed99f95e3fe884fb47b3cc09762
es-e2b <127708801+es-e2b@users.noreply.github.com>
2024-01-27 13:32:22 +0900

    [Gold IV] Title: 인구 이동, Time: 408 ms, Memory: 19320 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.cs
+++ 백준/Gold/16234. 인구 이동/인구 이동.cs
@@ -1,49 +1,55 @@
 #nullable disable
-using System.Globalization;
 using System.Text;
 public static class Program
 {
     static int[] dx=new int[]{1,-1,0,0};
     static int[] dy=new int[]{0,0,1,-1};
-    static StreamReader sr=new StreamReader(Console.OpenStandardInput(), bufferSize: 65536);
     static StreamWriter sw=new StreamWriter(Console.OpenStandardOutput(), bufferSize: 65536);
     static StringBuilder sb=new StringBuilder();
+    private static int Read()
+    {
+        int c, n = Console.Read() & 15;
+
+        while ((c = Console.Read()) > 32)
+        {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+
+        return n;
+    }
     public static void Main()
     {
-        int[] input=sr.ReadLine().Split(' ').Select(int.Parse).ToArray();
-        int[][] arr=new int[input[0]][];
+        int[] input=new int[]{Read(), Read(), Read()};
+        int[,] arr=new int[input[0],input[0]];
         for (int i = 0; i < input[0]; i++){
-            arr[i]=sr.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            for(int j=0;j<input[0];j++)
+                arr[i,j]=Read();
         }
-        Queue<int[]> q=new Queue<int[]>();
+        Queue<(int y, int x)> q=new Queue<(int y, int x)>();
         int day=0;
         while(true){
             bool flag=false;
-            bool[][] visited=new bool[input[0]][];
-            for (int k = 0; k < input[0]; k++)
-                visited[k]=new bool[input[0]];
+            bool[,] visited=new bool[input[0],input[0]];
             for(int i=0;i<input[0];i++){
                 for(int j=0;j<input[0];j++){
-                    if(visited[i][j])continue;
-                    q.Enqueue(new int[]{i,j});
-                    List<int[]> alience=new List<int[]>();
+                    if(visited[i,j])continue;
+                    q.Enqueue((i,j));
+                    List<(int y, int x)> alience=new List<(int y, int x)>();
                     int sum=0;
                     while(q.Count>0){
                         while(q.Count>0){
-                            int[] now=q.Dequeue();
-                            int x=now[1];
-                            int y=now[0];
-                            if(visited[y][x])continue;
-                            alience.Add(now);
-                            sum+=arr[y][x];
-                            visited[y][x]=true;
+                            (int y, int x)=q.Dequeue();
+                            if(visited[y,x])continue;
+                            alience.Add((y,x));
+                            sum+=arr[y,x];
+                            visited[y,x]=true;
                             for(int k=0;k<4;k++){
                                 int nx=x+dx[k];
                                 int ny=y+dy[k];
-                                if(ny<0||ny>=input[0]||nx<0||nx>=input[0]||visited[ny][nx]||
-                                    Math.Abs(arr[ny][nx]-arr[y][x])<input[1]||
-                                    Math.Abs(arr[ny][nx]-arr[y][x])>input[2])continue;
-                                q.Enqueue(new int[]{ny,nx});
+                                if(ny<0||ny>=input[0]||nx<0||nx>=input[0]||visited[ny,nx]||
+                                    Math.Abs(arr[ny,nx]-arr[y,x])<input[1]||
+                                    Math.Abs(arr[ny,nx]-arr[y,x])>input[2])continue;
+                                q.Enqueue((ny,nx));
                             }
                         }
                     }
@@ -51,8 +57,9 @@ public static class Program
                         continue;
                     flag=true;
                     int tmp=sum/alience.Count;
-                    foreach(int[] contry in alience){
-                        arr[contry[0]][contry[1]]=tmp;
+                    foreach((int y, int x) contry in alience){
+                        (int y, int x)=contry;
+                        arr[y,x]=tmp;
                     }
                 }
             }
