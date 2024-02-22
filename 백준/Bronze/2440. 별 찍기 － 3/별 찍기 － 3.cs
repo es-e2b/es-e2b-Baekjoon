639b1431e645856ac5614a3efd3338f5ff8e013c
es-e2b <127708801+es-e2b@users.noreply.github.com>
2024-02-22 21:45:36 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 64 ms, Memory: 5568 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.cs
@@ -0,0 +1,24 @@
+#nullable disable
+using System.Text;
+public static class Program
+{
+    static StreamReader sr=new StreamReader(Console.OpenStandardInput(), bufferSize: 65536);
+    static StreamWriter sw=new StreamWriter(Console.OpenStandardOutput(), bufferSize: 65536);
+    static StringBuilder sb=new StringBuilder();
+    static Func<int[]> ReadIntArray=()=>Array.ConvertAll(sr.ReadLine().Split(),int.Parse);
+    static Func<int> ReadInt=()=>int.Parse(sr.ReadLine());
+    public static void Main()
+    {
+        int n=ReadInt();
+        while(n-->0)
+        {
+            for(int i=n+1;i>0;i--)
+            {
+                sb.Append("*");
+            }
+            sb.AppendLine();
+        }
+        sw.WriteLine(sb);
+        sw.Flush();
+    }
+}
