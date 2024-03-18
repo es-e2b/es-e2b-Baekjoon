41ae4f4002c48a28dc1977a4734de349c23032bf
es-e2b <127708801+es-e2b@users.noreply.github.com>
2024-03-18 20:59:01 +0900

    [Bronze III] Title: 오븐 시계, Time: 228 ms, Memory: 18508 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		sc.close();
+		
+		int totalM = (a * 60) + b;
+		int finTotalM = totalM + c;
+		
+		int finH = finTotalM / 60 % 24;
+		int finM = finTotalM % 60;
+		
+		System.out.println(finH + " " + finM);
+		
+	}
+}
