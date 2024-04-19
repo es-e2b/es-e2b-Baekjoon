c5060b81741ef5ff11a2eb9f223cfa803741927e
es-e2b <127708801+es-e2b@users.noreply.github.com>
2024-04-19 18:01:59 +0900

    [Bronze IV] Title: 녹색거탑, Time: 124 ms, Memory: 14060 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		
+		int n = System.in.read() - 48;
+		System.out.println((int)Math.pow(2, n));
+		
+	}
+}
